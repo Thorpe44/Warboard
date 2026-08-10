3b0781f206a69558ac69f5fee0b7ad3bbb580530
Thorpe44 <elliot.tomlinson@btinternet.com>
2026-08-10 02:47:44 +0100

    EOD 9th of August Post clean up

--- CLEAN_WARBOARD_V44_3.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0CLEAN_WARBOARD_V44_3.ps1"
-endlocal
--- INSTALL_WARBOARD_V45_PRESENTATION.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0INSTALL_WARBOARD_V45_PRESENTATION.ps1"
-endlocal
